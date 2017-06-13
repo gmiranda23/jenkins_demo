1930d10a0dce38fa0d12c0f63f0c51754e703697
gmiranda23 <gmiranda@opscode.com>
2017-06-13 09:15:05 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# jenkins_demo
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'jenkins_demo'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures jenkins_demo'
+long_description 'Installs/Configures jenkins_demo'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/jenkins_demo/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/jenkins_demo' if respond_to?(:source_url)
