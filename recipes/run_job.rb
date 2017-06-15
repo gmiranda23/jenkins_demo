caa187f01428727b683bb9f85e0b11ae0f00a8f4
gmiranda23 <gmiranda@opscode.com>
2017-06-15 08:57:22 -0700

    repo attribute added

--- recipes/run_job.rb
+++ recipes/run_job.rb
@@ -4,10 +4,11 @@
 #
 # Copyright (c) 2017 George Miranda, All Rights Reserved.
 
-jenkins_job 'hab_demo' do
+jenkins_job 'hab_build' do
   parameters(
     'PKG' => node['jenkins_demo']['job_parameter']['pkg'],
-    'ORIGIN' => node['jenkins_demo']['job_parameter']['origin']
+    'ORIGIN' => node['jenkins_demo']['job_parameter']['origin'],
+    'REPO' => node['jenkins_demo']['job_parameter']['repo']
   )
   stream_job_output true
   wait_for_completion true
